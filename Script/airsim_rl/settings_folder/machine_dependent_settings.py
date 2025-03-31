96ba16e968f5e4848a136599742cce2dcf632e46
kellendeng <88022100+Gramkellen@users.noreply.github.com>
2025-03-31 14:53:48 +0800

    添加最新代码

---
+++ Script/airsim_rl/settings_folder/machine_dependent_settings.py
@@ -0,0 +1,4 @@
+json_file_addr = "D:/Files/_GraduationProject/airlearning-ue4/Content/JsonFiles/EnvGenConfig.json"
+game_file = "D:/Files/_GraduationProject/airlearning-ue4/AirLearning.uproject"
+unreal_host_shared_dir = ""
+unreal_exe_path = "E:/UE/UE_4.27/Engine/Binaries/Win64/UE4Editor.exe"
